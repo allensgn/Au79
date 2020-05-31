9add4f6d77a5350cbec2318e82c990bbc76c5bcc
Allen Tan <allen_tan@singaporeglobalnetwork.sg>
2020-05-31 12:34:37 +0000

    Update .gitlab-ci.yml, .gitlab-deploy.sh files

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash#Get servers list
 set -f
 echo "Deploy project on server $DEPLOY_SERVER"    
-ssh ubuntu@$DEPLOY_SERVER 'bash' < .cloneGitRepo.sh
+ssh ubuntu@$DEPLOY_SERVER 'bash' < cloneGitRepo.sh
