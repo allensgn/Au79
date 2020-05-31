7ea623d222c541d6ba4627d98aec32bf5228b780
Allen Tan <allen_tan@singaporeglobalnetwork.sg>
2020-05-31 12:32:42 +0000

    Update cloneGitRepo.sh

--- cloneGitRepo.sh
+++ cloneGitRepo.sh
@@ -7,4 +7,4 @@ rm -rf /home/bitnami/stack/nginx/html/internship2020
 
 cd /home/bitnami/stack/nginx/html
 
-git clone https://nginx:dxsxDn_aZ_RyJbNPDr85@gitlab.com/allen_tan/internship2020.git
+git clone https://Au79:T3PENUt1msHUHuTjyBdV@gitlab.com/allen_tan/Pb82.git
